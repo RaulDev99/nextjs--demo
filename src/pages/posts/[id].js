e4c6b662919e144afe962298d479ea63d31cfda1
RaulDev99 <raulskate1999@gmail.com>
2022-04-10 22:24:06 +0200

    another

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -1,54 +1,54 @@
-import Layout from '../../components/Borrarlayout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
+// import Layout from '../../components/Borrarlayout'
+// import { getAllPostIds, getPostData } from '../../lib/posts'
+// import Head from 'next/head'
+// import Date from '../../components/date'
+// import utilStyles from '../../styles/utils.module.css'
 
 
 
-export default function Post({ postData }) {
-  return (
-    <>
+// export default function Post({ postData }) {
+//   return (
+//     <>
     
-  {/* <Layout>
+//   <Layout>
 
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
+//       <Head>
+//         <title>{postData.title}</title>
+//       </Head>
 
       
 
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
+//       <article>
+//         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+//         <div className={utilStyles.lightText}>
+//           <Date dateString={postData.date} />
+//         </div>
+//         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+//       </article>
       
-  </Layout> */}
-  </>
-  )
+//   </Layout>
+//   </>
+//   )
   
-}
+// }
 
 
-export async function getStaticPaths() {
-    const paths = getAllPostIds()
-    return {
-      paths,
-      fallback: false
-    }
-  }
+// export async function getStaticPaths() {
+//     const paths = getAllPostIds()
+//     return {
+//       paths,
+//       fallback: false
+//     }
+//   }
 
-export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id)
-    return {
-      props: {
-        postData
-      }
-    }
-  }
+// export async function getStaticProps({ params }) {
+//     const postData = await getPostData(params.id)
+//     return {
+//       props: {
+//         postData
+//       }
+//     }
+//   }
 
 
 
