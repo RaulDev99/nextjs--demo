932b715c140fc896f2960d16d9cb76f9432af07f
RaulDev99 <raulskate1999@gmail.com>
2022-04-14 01:35:18 +0200

    14042022 2

--- src/pages/registro2.js
+++ src/pages/registro2.js
@@ -34,8 +34,10 @@ export default function Registro2 (){
         </div>
         <div className="w-full h-0.5 bg-gray-300 "></div>
 
-        {/* <SubMenuFecha></SubMenuFecha> */}
-            <div className='grid grid-cols-5 text-center text-xs mt-4 font-bold text-gray-600 mx-2'>
+        {/* SUBMENU FECHA */}
+
+        { menuTopActive === 'Fecha' ? 
+            <div className='grid grid-cols-5 text-center text-sm mt-4 font-bold text-gray-600 mx-2'>
                 <h2 onClick={toggleSubMenuFechaActive} className={subMenuFechaActive === 'Todos'  ? 'bg-blue-400 rounded-md p-2 transition-all': 'm-2'}>Todos</h2>
                 <h2 onClick={toggleSubMenuFechaActive} className={subMenuFechaActive === 'Diario'  ? 'bg-blue-400 rounded-md p-2 transition-all': 'm-2'}>Diario</h2>
                 <h2 onClick={toggleSubMenuFechaActive} className={subMenuFechaActive === 'Semanal'  ? 'bg-blue-400 rounded-md p-2 transition-all': 'm-2'}>Semanal</h2>
@@ -44,14 +46,21 @@ export default function Registro2 (){
                     <div className=' relative z-10'>
                     <h2 onClick={toggleSubMenuFechaActive} className={subMenuFechaActive === 'Calendario'  ? 'bg-blue-400 rounded-md p-2 transition-all ': 'm-2'}>Calendario</h2>
                     </div>
-                    <CalendarIcon onClick={toggleSubMenuFechaActive} className={subMenuFechaActive === 'Calendario'  ? ' none ': 'h-12 absolute opacity-20 bg-green '} ></CalendarIcon>
+                    <CalendarIcon onClick={toggleSubMenuFechaActive} className={subMenuFechaActive === 'Calendario'  ? 'hidden': 'h-12 absolute opacity-20'} ></CalendarIcon>
                 </div>
             </div>
+            : null
+            }
+            
+            {/* SUBMENU FECHA>TODOS */}
+            
+            { menuTopActive === 'Fecha' && subMenuFechaActive  === 'Todos' ? 
             <div className='grid grid-cols-2 text-center font-bold text-gray-600 mt-7'>
                 <h2 onClick={toggleSubMenuTodosActive} className={subMenuTodosActive === 'Mas reciente'  ? 'bg-gray-700 text-white rounded-sm p-2  transition-all': 'm-2'} >Mas reciente</h2>
                 <h2 onClick={toggleSubMenuTodosActive} className={subMenuTodosActive === 'Mas antiguo'  ? 'bg-gray-700 text-white rounded-sm p-2 transition-all': 'm-2'}>Mas antiguo</h2>
             </div>
-
+            : null
+            }
             
             
 
